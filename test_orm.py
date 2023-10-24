f36d075734272650c211845e6b35ed30add6ac13
Jing C <jing@Jings-MacBook-Pro>
2023-10-24 15:58:54 -0400

    update orm tests

--- test_orm.py
+++ test_orm.py
@@ -3,7 +3,8 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 from model import OrderLine
-from orm import start_mappers, metadata 
+from orm import start_mappers, metadata
+
 
 @pytest.fixture
 def in_memory_db():
@@ -11,17 +12,18 @@ def in_memory_db():
     metadata.create_all(engine)
     return engine
 
+
 @pytest.fixture
 def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db)()
     clear_mappers()
 
-# @pytest.mark.skip
-def test_orderline_mapper_can_load_lines(session):  #(1)
 
+# @pytest.mark.skip
+def test_orderline_mapper_can_load_lines(session):  # (1)
     session.execute(
-        "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+        "INSERT INTO order_lines(orderid, sku, qty) VALUES "
         '("order1", "RED-CHAIR", 12),'
         '("order1", "RED-TABLE", 13),'
         '("order2", "BLUE-LIPSTICK", 14)'
@@ -31,10 +33,9 @@ def test_orderline_mapper_can_load_lines(session):  #(1)
         OrderLine("order1", "RED-TABLE", 13),
         OrderLine("order2", "BLUE-LIPSTICK", 14),
     ]
-    # result = session.query(OrderLine).all()
-    # print(f"result: {result}")
 
-    assert session.query(OrderLine).all() == expected
+    result = session.query(OrderLine).all()
+    assert result == expected
 
 
 def test_orderline_mapper_can_save_lines(session):
